1dab4b217a6aabef3d516c2c56a7bc6becdc6645
Viktor Karlsson <vkarlsso@DESKTOP-4NJ0PIQ.localdomain>
2023-12-02 06:26:58 +0100

    Solved day 2

--- 2023/02/p1.py
+++ 2023/02/p1.py
@@ -1,1 +1,38 @@
-from ACutils.utils import read_input_to_list_of_strings
+from ACutils.utils import read_input_to_list_of_strings, replace_text_with_digit
+
+from typing import List
+
+MAX_RED = 12
+MAX_GREEN = 13
+MAX_BLUE = 14
+
+def is_set_possible(sets: List[str]):
+    for set in sets:
+        number_colors = set.split(',')
+        for cube_ammount in number_colors:
+            tmp = cube_ammount.strip()
+            number = replace_text_with_digit(tmp)
+            if "blue" in tmp:
+                if int(number) > MAX_BLUE:
+                    return False
+            if "red" in tmp:
+                if int(number) > MAX_RED:
+                    return False
+            if "green" in tmp:
+                if int(number) > MAX_GREEN:
+                    return False
+    return True
+
+
+def main():
+    input = read_input_to_list_of_strings("input.txt")
+    total = 0
+    for game in input:
+        game_id = game.split(':')[0].replace("Game ", "")
+        sets = game.split(':')[1].split(';')
+        if is_set_possible(sets):
+            total += int(game_id)
+    print("Result: {}".format(total))
+
+
+main()
--- 2023/02/p2.py
+++ 2023/02/p2.py
@@ -1,1 +1,37 @@
-from ACutils.utils import read_input_to_list_of_strings
+from ACutils.utils import read_input_to_list_of_strings, replace_text_with_digit
+
+from typing import List
+
+MAX_RED = 12
+MAX_GREEN = 13
+MAX_BLUE = 14
+
+def get_power(sets: List[str]) -> int:
+    min_red = 0
+    min_green = 0
+    min_blue = 0
+    for set in sets:
+        number_colors = set.split(',')
+        for cube_ammount in number_colors:
+            tmp = cube_ammount.strip()
+            number = int(replace_text_with_digit(tmp))
+            if "blue" in tmp:
+                min_blue = max(min_blue, number)
+            if "red" in tmp:
+                min_red = max(min_red, number)
+            if "green" in tmp:
+                min_green = max(min_green, number)
+
+    return int(min_red * min_green * min_blue)
+
+
+def main():
+    input = read_input_to_list_of_strings("input.txt")
+    total = 0
+    for game in input:
+        sets = game.split(':')[1].split(';')
+        total += int(get_power(sets))
+    print("Result: {}".format(total))
+
+
+main()
