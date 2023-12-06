31c8eee9213d3fee1507b02e26b34ae709b9d0e6
Viktor Karlsson <vkarlsso@DESKTOP-4NJ0PIQ.localdomain>
2023-12-06 06:35:52 +0100

    Solved day 6

--- 2023/06/p1.py
+++ 2023/06/p1.py
@@ -1,1 +1,24 @@
 from ACutils.utils import read_input_to_list_of_strings
+
+
+def main():
+    input_list = read_input_to_list_of_strings("input.txt")
+
+    times = [int(x) for x in input_list[0].split() if x.isdigit()]
+    distances = [int(x) for x in input_list[1].split() if x.isdigit()]
+    number_of_ways = []
+
+    for i, milliseconds in enumerate(times):
+        goal_distance = distances[i]
+        ways = 0
+        for holding in range(milliseconds + 1):
+            if holding * (milliseconds - holding) > goal_distance:
+                ways += 1
+        number_of_ways.append(ways)
+
+    result = 1
+    for x in number_of_ways:
+        result *= x
+    print(result)
+
+main()
--- 2023/06/p2.py
+++ 2023/06/p2.py
@@ -1,1 +1,20 @@
 from ACutils.utils import read_input_to_list_of_strings
+
+
+
+def main():
+    input_list = read_input_to_list_of_strings("input.txt")
+
+    times = [int(x) for x in input_list[0].split() if x.isdigit()]
+    distances = [int(x) for x in input_list[1].split() if x.isdigit()]
+
+    milliseconds = int("".join([str(x) for x in times]))
+    goal_distance = int("".join([str(x) for x in distances]))
+    ways = 0
+    for holding in range(milliseconds + 1):
+        if holding * (milliseconds - holding) > goal_distance:
+            ways += 1
+
+    print(ways)
+
+main()
