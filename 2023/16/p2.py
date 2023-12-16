a143c20706e31d56fa634b12576807386d67771c
Viktor Karlsson <vkarlsso@DESKTOP-4NJ0PIQ.localdomain>
2023-12-16 06:58:43 +0100

    Solved day 16

--- 2023/16/p2.py
+++ 2023/16/p2.py
@@ -1,1 +1,52 @@
-from ACutils.utils import read_input_to_list_of_strings
+from ACutils.utils import read_input_to_grid_2d
+
+from typing import Tuple, List
+
+
+def fire(input: List[str], beam: Tuple[Tuple[int, int], Tuple[int, int]]) -> int:
+    energized = set()
+    beams = [beam]
+
+    while any(beams):
+        for i, beam in enumerate(beams):
+            if not beam:
+                continue
+            (x, y), (dx, dy) = beam
+            if beam in energized:
+                del beams[i]
+            else:
+                energized.add(beam)
+                x += dx
+                y += dy
+                if min(x, y) < 0 or y >= len(input) or x >= len(input[0]):
+                    # out of bounds
+                    del beams[i]
+                    continue
+                cell = input[y][x]
+                if cell == '/':
+                    dx, dy = -dy, -dx
+                elif cell == '\\':
+                    dx, dy = dy, dx
+                elif cell == '-' and dy:
+                    dx, dy = 1, 0
+                    beams.append(((x, y), (-1, 0)))
+                elif cell == '|' and dx:
+                    dx, dy = 0, 1
+                    beams.append(((x, y), (0, -1)))
+                beams[i] = (x, y), (dx, dy)
+
+    return len({p for p, _ in energized}) - 1
+
+
+def main():
+
+
+    input = read_input_to_grid_2d("input.txt")
+    print('Result', max([*(fire(input, ((i, -1), (0, 1))) for i in range(len(input[0]))),
+                          *(fire(input, ((i, len(input)), (0, -1))) for i in range(len(input[0]))),
+                          *(fire(input, ((-1, i), (1, 0))) for i in range(len(input))),
+                          *(fire(input, ((len(input[0]), i), (-1, 0))) for i in range(len(input)))
+                        ]))
+
+
+main()
