dd2fb5f7eb9afa5478d730405b9ce9866ea43a41
Viktor Karlsson <zartockeu@gmail.com>
2023-12-20 13:25:09 +0100

    Solved day 20

--- 2023/20/p1.py
+++ 2023/20/p1.py
@@ -1,1 +1,46 @@
-from ACutils.utils import read_input_to_list_of_strings
+from collections import defaultdict
+from math import prod
+
+
+modules = dict()
+flips = defaultdict(int)
+conjs = defaultdict(dict)
+
+for line in open('input.txt'):
+    tmp, _, *destination_modules = line.replace(',', '').split()
+    prefix, module = (tmp[0], tmp[1:]) if tmp[0] in '%&' else ('', tmp)
+
+    modules[module] = prefix, destination_modules
+
+presses = 0
+counts = [0, 0]
+
+while True:
+    if presses == 1000:
+        print(prod(counts))
+        break
+    presses += 1
+
+    queue = [(None, 'broadcaster', 0)]
+    while queue:
+        source, module, pulse_in = queue.pop(0)
+        counts[pulse_in] += 1
+
+        if module not in modules:
+            continue
+        type, nexts = modules[module]
+
+        match type, pulse_in:
+            case '', _:
+                # broadcaster
+                pulse_out = pulse_in
+            case '%', 0:
+                pulse_out = flips[module] = not flips[module]
+            case '&', _:
+                conjs[module][source] = pulse_in
+                pulse_out = not all(conjs[module].values())
+            case _, _:
+                continue
+
+        for n in nexts:
+            queue.append((module, n, pulse_out))
--- 2023/20/p2.py
+++ 2023/20/p2.py
@@ -1,1 +1,56 @@
-from ACutils.utils import read_input_to_list_of_strings
+from collections import defaultdict
+from math import prod
+
+
+modules = dict()
+flips = defaultdict(int)
+conjs = defaultdict(dict)
+
+for line in open('input.txt'):
+    tmp, _, *destination_modules = line.replace(',', '').split()
+    prefix, module = (tmp[0], tmp[1:]) if tmp[0] in '%&' else ('', tmp)
+
+    modules[module] = prefix, destination_modules
+
+    for destination in destination_modules:
+        conjs[destination][tmp] = 0
+        if destination == 'rx': rx = tmp
+
+rx_ins = {i: 0 for i in conjs[rx]}
+
+presses = 0
+counts = [0, 0]
+
+while True:
+    presses += 1
+
+    if all(rx_ins.values()):
+        print(prod(rx_ins.values()))
+        break
+
+    queue = [(None, 'broadcaster', 0)]
+    while queue:
+        source, mod, pulse_in = queue.pop(0)
+        counts[pulse_in] += 1
+
+        if mod not in modules: continue
+        type, nexts = modules[mod]
+
+        match type, pulse_in:
+            case '', _:
+                #brodcaster
+                pulse_out = pulse_in
+            case '%', 0:
+                pulse_out = flips[mod] = not flips[mod]
+            case '&', _:
+                conjs[mod][source] = pulse_in
+                pulse_out = not all(conjs[mod].values())
+
+                if 'rx' in nexts:
+                    for key, value in conjs[mod].items():
+                        if value:
+                            rx_ins[key] = presses
+            case _,_: continue
+
+        for n in nexts:
+            queue.append((mod, n, pulse_out))
