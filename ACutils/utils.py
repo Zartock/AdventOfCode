bb6bd6e08ad5d23a2f02be76924972057ad4fb4c
Viktor Karlsson <vika@Viktor-desktop>
2023-12-23 12:34:50 +0100

    Solved day 23

--- ACutils/utils.py
+++ ACutils/utils.py
@@ -1,6 +1,7 @@
 import os
 import inspect
 from typing import List, Tuple
+from enum import Enum
 
 DIGITS_TEXT_MAPPING = {"one": 1,
                        "two": 2,
@@ -11,6 +12,15 @@ DIGITS_TEXT_MAPPING = {"one": 1,
                        "seven": 7,
                        "eight": 8,
                        "nine": 9}
+
+class Cardinal(Enum):
+    __order__ = 'WEST EAST NORTH SOUTH'
+    WEST = (-1, 0)
+    EAST = (1, 0)
+    NORTH = (0, -1)
+    SOUTH = (0, 1) 
+
+CARDINALS = [x.value for x in Cardinal]
 
 
 def read_input_to_list_of_strings(file_name: str) -> List[str]: 
@@ -66,3 +76,33 @@ def get_adjacent_values_in_matrix_for_range(matrix: List[List[str]], coord_range
                     value_to_add = matrix[row][col]
                     adjacent_values.append(value_to_add)
     return adjacent_values
+
+def in_range(i, j, max_i, max_j):
+    return i >= 0 and j >= 0 and i < max_i and j < max_j
+
+def neighbors_helper(max_i, max_j, candidates):
+    if max_j is None and isinstance(max_i, list):
+        max_j = len(max_i[0])
+        max_i = len(max_i)
+    return [(i, j) for i, j in candidates if in_range(i, j, max_i, max_j)]
+
+def cardinal_neighbors(i, j, max_i, max_j=None):
+    return neighbors_helper(
+        max_i, max_j, [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]
+    )
+
+def all_neighbors(i, j, max_i, max_j=None):
+    return neighbors_helper(
+        max_i,
+        max_j,
+        [
+            (i - 1, j),
+            (i + 1, j),
+            (i, j - 1),
+            (i, j + 1),
+            (i - 1, j - 1),
+            (i - 1, j + 1),
+            (i + 1, j - 1),
+            (i + 1, j + 1),
+        ],
+    )
